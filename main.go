4b6a44489617106a4d4afa8b176bcacd1a074d57
detect <dmitry.bobrovitsky@gmail.com>
2017-08-22 13:28:45 +0300

    add percent of hard and soft bounces

--- main.go
+++ main.go
@@ -7,21 +7,86 @@ import (
     "net"
     "io"
     "io/ioutil"
+    "math/rand"
+    "time"
+    "fmt"
 )
 
 var (
     listen = "0.0.0.0:25"
+    hard_percent = 5
+    soft_percent = 15
 )
+
+var hard_bounce = []string{
+    "554 rejected due to spam content",
+    "550 deliver error: dd This user doesn't have yahoo.com account",
+    "554 RLY:B1",
+    "554 Invalid recipient",
+    "554 Message permanently rejected",
+}
+
+var soft_bounce = []string{
+    "421 Service not available",
+    "421 Mailbox unavailable",
+    "452 Insufficient system storage",
+    "452 Mailbox full",
+}
+
+const (
+    step_connect = iota
+    step_hello
+    step_sender
+    step_recipient
+    step_message
+)
+
+type Answer struct {
+    step int
+    resp error
+    num int
+}
 
 type Session struct {
 	Conn net.Conn
+    Answer *Answer
+}
+
+func GetAnswer() *Answer {
+    var num = rand.Intn(100)
+
+    if hard_percent >= num {
+        return &Answer {
+            step: rand.Intn(5),
+            resp: fmt.Errorf(hard_bounce[rand.Intn(len(hard_bounce))]),
+            num: num,
+        }
+    }
+
+    if num > hard_percent && num <= (soft_percent + hard_percent) {
+        return &Answer {
+            step: rand.Intn(5),
+            resp: fmt.Errorf(soft_bounce[rand.Intn(len(soft_bounce))]),
+            num: num,
+        }
+    }
+
+    return nil
 }
 
 func (s *Session) Connect(source string) error {
+    if s.Answer != nil && s.Answer.step == 0 {
+        return s.Answer.resp
+    }
+
     return nil
 }
 
 func (s *Session) Hello(hostname string) error {
+    if s.Answer != nil && s.Answer.step == 1 {
+        return s.Answer.resp
+    }
+
     return nil
 }
 
@@ -33,19 +98,34 @@ func (s *Session) AuthSuccess(username string) {
 }
 
 func (s *Session) Sender(address string) error {
+    if s.Answer != nil && s.Answer.step == 2 {
+        return s.Answer.resp
+    }
+
     return nil
 }
 
 func (s *Session) Recipient(address string) error {
+    if s.Answer != nil && s.Answer.step == 3 {
+        return s.Answer.resp
+    }
+
     return nil
 }
 
 func (s *Session) Message(reader io.Reader) error {
     io.Copy(ioutil.Discard, reader)
+
+    if s.Answer != nil && s.Answer.step == 4 {
+        return s.Answer.resp
+    }
+
 	return nil
 }
 
 func main() {
+
+    rand.Seed(time.Now().UTC().UnixNano())
 
     srv := smtpd.Server {
         Hostname: "fake-smtpd",
@@ -72,6 +152,7 @@ func main() {
 
             session := &Session{
                 Conn: conn,
+                Answer: GetAnswer(),
             }
 
             srv.ServeSMTP(conn, session)
